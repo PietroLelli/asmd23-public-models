de5bf59698f5e802532c3d53f5cd4ad3fdfb8905
Pietro Lelli <pietro.lelli00@gmail.com>
2024-04-10 16:13:31 +0200

    implement RW PetriNet

---
+++ src/main/scala/u06/task1/ReadersWritersPetriNet.scala
@@ -0,0 +1,74 @@
+package scala.u06.task1
+
+export pc.modelling.PetriNet
+import pc.utils.MSet
+
+object ReadersWritersPetriNet:
+  enum Place:
+    case Idle, ChooseAction, ReadyToRead, ReadyToWrite, Reading, Writing, HasPermission
+
+  export Place.*
+  export pc.modelling.PetriNet.*
+  export pc.modelling.SystemAnalysis.*
+  export pc.utils.MSet
+
+  def petriNetRW = PetriNet[Place](
+    MSet(Idle) ~~> MSet(ChooseAction),
+    MSet(ChooseAction) ~~> MSet(ReadyToRead),
+    MSet(ChooseAction) ~~> MSet(ReadyToWrite),
+    MSet(ReadyToRead, HasPermission) ~~> MSet(Reading, HasPermission),
+    MSet(Reading) ~~> MSet(Idle),
+    MSet(ReadyToWrite, HasPermission) ~~> MSet(Writing) ^^^ MSet(Reading),
+    MSet(Writing) ~~> MSet(Idle, HasPermission)
+  ).toSystem
+  
+  def isMutuallyExclusive(initialState: MSet[Place], depth: Int): Boolean =
+    val statesMutualExclusion: Seq[Boolean] =
+      for
+        p <- petriNetRW.paths(initialState, depth)
+        s <- p
+      yield s.diff(MSet(Reading, Writing)).size == s.size - 2 || s.diff(MSet(Writing, Writing)).size == s.size - 2
+  
+    !statesMutualExclusion.contains(true)
+  /*val paths = pnRW.paths(initialState, depth)
+  !paths.exists { path =>
+    path.exists { state =>
+      val diffReadingWriting = state.diff(MSet(Reading, Writing)).size
+      val diffWritingWriting = state.diff(MSet(Writing, Writing)).size
+      diffReadingWriting == state.size - 2 || diffWritingWriting == state.size - 2
+    }
+  }*/
+  //pnRW.paths(initialState, depth).flatMap(p => p.filter(s => s.diff(MSet(Reading, Writing)).size == s.size - 2 || s.diff(MSet(Writing, Writing)).size == s.size - 2)).isEmpty
+  
+  def isReachable(initialState: MSet[Place], depth: Int): Boolean =
+    val allReachedStates: Seq[Place] =
+      for
+        path <- petriNetRW.paths(initialState, depth)
+        state <- path
+        place <- state.asList
+      yield place
+  
+    allReachedStates.toSet == Place.values.toSet
+  
+  def maxTokenInPN(initialState: MSet[Place]): Int =
+    if initialState.matches(MSet(HasPermission)) then initialState.size else initialState.size + 1
+  
+  def isBounded(initialState: MSet[Place], depth: Int): Boolean =
+    val maxSize = maxTokenInPN(initialState)
+    val allReachedStates: Seq[Boolean] =
+      for
+        path <- petriNetRW.paths(initialState, depth)
+        state <- path
+      yield state.size <= maxSize
+  
+    allReachedStates.forall(identity)
+  
+  def isBounded2(initialState: MSet[Place], depth: Int): Boolean =
+    (for
+      path: Path[Marking[Place]] <- petriNetRW.paths(initialState, depth)
+      state <- path
+    yield state.size <= maxTokenInPN(initialState)).reduce(_ && _)
+
+
+  @main def mainPNMutualExclusion =
+    println(petriNetRW.paths(MSet(Idle, Idle, HasPermission), 3).toList.mkString("\n"))
---
+++ src/test/scala/u06/task1/ReadersWritersPetriNetTest.scala
@@ -0,0 +1,20 @@
+package scala.u06.task1
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.be
+import org.scalatest.matchers.should.Matchers.shouldBe
+
+class ReadersWritersPetriNetTest extends AnyFunSuite:
+  import ReadersWritersPetriNet.*
+
+  test("Mutual exclusion"):
+    isMutuallyExclusive(MSet(Idle, Idle, HasPermission), 10) shouldBe true
+
+  test("Not in mutual exclusion"):
+    isMutuallyExclusive(MSet(Idle, Idle, Reading, Writing), 10) shouldBe false
+
+  test("Reachability"):
+    isReachable(MSet(Idle, Idle, HasPermission), 10) shouldBe true
+
+  test("Boundness"):
+    isBounded2(MSet(Idle, Idle, HasPermission), 10) shouldBe true
