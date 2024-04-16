b85e59d1fad3580b260590c50ed08ed4a309cf99
Pietro Lelli <pietro.lelli00@gmail.com>
2024-04-16 14:10:25 +0200

    complete task1

--- src/main/scala/u07/examples/StochasticChannelSimulation.scala
+++ src/main/scala/u07/examples/StochasticChannelSimulation.scala
@@ -11,3 +11,27 @@ import u07.examples.StochasticChannel.*
         .take(10)
         .toList
         .mkString("\n")
+
+@main def main =
+  println("AVG time stochastic channel: " + averageTimeStochasticChannel(10))
+  println("Percentage time in fail state: "+ percentageTimeInFailState(10))
+
+def averageTimeStochasticChannel(nRun: Int): Double =
+  (0 to nRun).foldLeft(0.0)((z, t) =>
+    z + stocChannel.newSimulationTrace(IDLE, new Random)
+      .take(10)
+      .toList
+      .find(e => e._2 == DONE)
+      .map(_.time)
+      .getOrElse(0.0)) / nRun
+
+def percentageTimeInFailState(nRun: Int): Double =
+  val totalTimes = (0 to nRun).foldLeft((0.0, 0.0)) ((acc, _) => {
+    val (failTime, totTime) = stocChannel.newSimulationTrace(IDLE, new Random)
+    .take(10)
+    .toList
+    .sliding(2)
+    .foldLeft((0.0, 0.0))((z, s) => if s(0).state == FAIL then (z._1 + s(1).time - s(0).time, s(1).time) else (z._1, s(1).time))
+    (acc._1 + failTime, acc._2 + totTime)
+  })
+  totalTimes._1 / totalTimes._2
