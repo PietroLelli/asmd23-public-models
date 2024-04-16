433cb21db21b1b0e1691deed81c09b024ef24275
Pietro Lelli <pietro.lelli00@gmail.com>
2024-04-16 21:49:14 +0200

    add simulation chart

--- build.sbt
+++ build.sbt
@@ -7,7 +7,8 @@ lazy val root = (project in file("."))
     name := "course-asmd23-models",
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "3.2.12" % Test,
-      "org.scalacheck" %% "scalacheck" % "1.17.0"
+      "org.scalacheck" %% "scalacheck" % "1.17.0",
+      "org.knowm.xchart" % "xchart" % "3.8.0"
     ),
     Compile / scalaSource := baseDirectory.value / "src" / "main",
     Test / scalaSource := baseDirectory.value / "src" / "test",
--- src/main/scala/u07/task2/BrusselatorPetriNet.scala
+++ src/main/scala/u07/task2/BrusselatorPetriNet.scala
@@ -4,6 +4,8 @@ import u07.modelling.SPN
 import u07.utils.MSet
 
 import java.util.Random
+import org.knowm.xchart.{QuickChart, SwingWrapper, XYChart}
+
 
 object BrusselatorPetriNet:
   enum Place:
@@ -22,9 +24,15 @@ object BrusselatorPetriNet:
   @main def main =
     val simulation = toCTMC(brusselatorPetriNet).newSimulationTrace(MSet(A,B,B,B,X,Y), new Random)
       .take(10)
+      .toList
+    simulation.foreach(println)
 
-    simulation.foreach(e => {
-        println("time: " + e._1)
-        println("X: " + e._2.countOccurrences(X))
-        println("Y: " + e._2.countOccurrences(Y))
-      })
+    val times = simulation.map(_._1).toArray
+    val xCounts = simulation.map(_._2.countOccurrences(X)).map(_.toDouble).toArray
+    val yCounts = simulation.map(_._2.countOccurrences(Y)).map(_.toDouble).toArray
+
+    val chart = QuickChart.getChart("Brusselator Simulation", "Time", "Count", "X", times, xCounts)
+    chart.addSeries("Y", times, yCounts)
+    chart.getStyler.setLegendVisible(true)
+
+    new SwingWrapper[XYChart](chart).displayChart().setTitle("Brusselator Simulation")
